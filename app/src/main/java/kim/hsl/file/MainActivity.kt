79cfc7ee133dcdd05795b188e79587c21a11cf03
hanshuliang <octopus_truth@163.com>
2021-10-11 17:35:01 +0800

    update

--- app/src/main/java/kim/hsl/file/MainActivity.kt
+++ app/src/main/java/kim/hsl/file/MainActivity.kt
@@ -28,6 +28,14 @@ class MainActivity : AppCompatActivity() {
         // 动态权限获取
         doSomethingWithPermissions()
 
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            Environment.isExternalStorageLegacy()
+        }else{
+
+        }
+
+
     }
 
     @AfterPermissionGranted( 100 )
